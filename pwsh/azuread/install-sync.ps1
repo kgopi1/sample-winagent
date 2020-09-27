a6cfe358acbeb5544c5e17d6ddcfe50c44904a75
MOKSHI <k.gopi1@gmail.com>
2020-09-27 17:06:26 +0100

    added the function

--- pwsh/azuread/install-sync.ps1
+++ pwsh/azuread/install-sync.ps1
@@ -1,7 +1,12 @@
-function install-azmodule(name) {
+function install-module {
+    param (
+  
+        [string]$ModuleName
+    )
+
     try {
         Install-PackageProvider -Name Nuget -Confirm:$false -Force 
-        Install-Module -Name $name -Confirm:$false -Force
+        Install-Module -Name $ModuleName -Confirm:$false -Force
     }
     catch {
         throw "unable to intall the modules"
