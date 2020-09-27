51f7e58b2eac9648d5ce81286b78bea9425853c6
MOKSHI <k.gopi1@gmail.com>
2020-09-27 11:04:19 +0100

    added azuread modules

--- pwsh/azuread/install-sync.ps1
+++ pwsh/azuread/install-sync.ps1
@@ -1,1 +1,22 @@
-Install-Module -Name AzureAD
+
+
+
+
+
+
+function install-azmodule {
+    try {
+        Install-PackageProvider -Name Nuget -Confirm:$false -Force 
+        Install-Module -Name AzureAD -Confirm:$false -Force
+    }
+    catch {
+        throw "unable to intall the modules"
+        
+    }
+    
+}
+
+
+
+## main 
+install-azmodule
